3dbc98aad4f7e0da77fdf6e130369467ee55a054
Synfiv <synfiv@gmail.com>
2024-09-09 20:27:58 +0200

    Tried to make a page with Quasar

---
+++ src/layouts/MainHeader.vue
@@ -0,0 +1,74 @@
+<template>
+  <q-layout view="lHh lpr lFf">
+    <div class="row justify-center">
+      <q-header class="">
+        <div class="flex items-stretch text-grey-10 bg-secondary">
+          <div class="flex justify-end row-grow">
+            <div class="flex q-px-lg text-h4 row-1">Saru</div>
+          </div>
+          <div class="flex justify-start row-1 row-grow">
+            <q-tabs v-model="tab" shrink stretch>
+              <q-route-tab to="/" label="Main Page" />
+              <q-route-tab to="/AboutPage" label="About" />
+              <q-route-tab to="/PricingPage" label="Pricing" />
+            </q-tabs>
+          </div>
+        </div>
+      </q-header>
+    </div>
+
+    <!-- (Optional) The Footer -->
+    <q-footer class="bg-accent q-pb-md q-pt-sm">
+      <q-tabs v-model="tab">
+        <div class="row q-gutter-lg">
+          <q-btn
+            icon="img:src/assets/website/twitter.svg"
+            href="https://twitter.com/SynfIV"
+            target="_blank"
+            size="max-content"
+            label="Twitter"
+            no-caps
+            outline
+            class="col-shrink"
+          />
+          <q-btn
+            icon="img:src/assets/website/bluesky.svg"
+            href="https://bsky.app/profile/synfiv.bsky.social"
+            target="_blank"
+            size="max-content"
+            label="Bluesky"
+            no-caps
+            outline
+            class="col-shrink"
+          />
+          <q-btn
+            icon="img:src/assets/website/furaffinity.svg"
+            href="https://www.furaffinity.net/user/synfiv"
+            target="_blank"
+            size="max-content"
+            label="Furaffinity"
+            no-caps
+            outline
+            class="col-shrink"
+          />
+        </div>
+      </q-tabs>
+    </q-footer>
+    <q-page-container>
+      <!-- This is where pages get injected -->
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    return {
+      tab: ref("images"),
+    };
+  },
+};
+</script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,15 +1,88 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page class="text-brand q-mx-xl">
+    <div class="">
+      <div class="row justify-evenly items-center bg-dark">
+        <div class="column text-center q-pa-md col-xs-12 col-sm-12 col-md-5">
+          <div class="text-h4 q-pa-md text-primary">
+            Hi, I am Saru. I am an artist in the process of expanding my hobby.
+          </div>
+          <q-separator size="4px" color="grey" inset />
+          <div class="text-body1 q-pa-md">
+            I'm an artist who has been working with various forms of mostly 3D
+            art for a few years now, and have recently began to focus on art
+            more seriously.
+          </div>
+          <div class="text-h5 q-pa-md">
+            <p>
+              I am currently:
+              <span class="bg-black q-pa-xs rounded-borders text-green"
+                >Accepting</span
+              >
+              Commissions
+            </p>
+          </div>
+        </div>
+        <q-separator size="4px" color="grey" inset vertical />
+        <div class="q-pa-md col-xs-12 col-sm-12 col-md-5">
+          <q-carousel
+            animated
+            v-model="slide"
+            navigation
+            infinite
+            height="max-content"
+            autoplay="autoplay"
+            arrows
+            transition-prev="slide-right"
+            transition-next="slide-left"
+            @mouseenter="autoplay = false"
+            @mouseleave="autoplay = true"
+            class="flex-center bg-accent"
+          >
+            <q-carousel-slide name="1" class="flex justify-center">
+              <q-img
+                src="src/assets/gallery/art1.jpg"
+                style="max-width: 75%"
+                class="border"
+              />
+            </q-carousel-slide>
+            <q-carousel-slide name="2" class="flex justify-center">
+              <q-img
+                src="src/assets/gallery/art2.jpg"
+                style="max-width: 75%"
+                class="border"
+              />
+            </q-carousel-slide>
+            <q-carousel-slide name="3" class="flex justify-center">
+              <q-img
+                src="src/assets/gallery/art3.jpg"
+                style="max-width: 75%"
+                class="border"
+              />
+            </q-carousel-slide>
+            <q-carousel-slide name="4" class="flex justify-center">
+              <q-img
+                src="src/assets/gallery/art4.jpg"
+                style="max-width: 75%"
+                class="border"
+              />
+            </q-carousel-slide>
+          </q-carousel>
+        </div>
+      </div>
+    </div>
   </q-page>
 </template>
 
-<script setup>
-defineOptions({
-  name: 'IndexPage'
-});
+<script>
+import { ref } from "vue";
+
+export default {
+  name: "IndexPage",
+  setup() {
+    return {
+      slide: ref(1),
+      autoplay: ref(true),
+    };
+  },
+};
 </script>
