d689b7cf836218a5b0ebd4f276d1d1c950fd1efa
Synfiv <synfiv@gmail.com>
2024-10-02 21:17:37 +0200

    extract me from this suffering

---
+++ src/pages/ErrorNotFound.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    class="fullscreen bg-primary text-accent text-center q-pa-md flex flex-center"
+  >
+    <div>
+      <div style="font-size: 30vh">404</div>
+
+      <div class="text-h1">Not much to see here...</div>
+
+      <q-btn
+        class="q-mt-xl"
+        color="secondary"
+        text-color="black"
+        unelevated
+        to="/"
+        label="Go Back?"
+        no-caps
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineOptions({
+  name: "ErrorNotFound",
+});
+</script>
